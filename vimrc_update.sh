07701dfceb9f1f12d8026a0b84cca9741a476ecd
srosendal <37482030+srosendal@users.noreply.github.com>
2020-04-05 13:27:46 +0200

    Update vimrc_update.sh

--- vimrc_update.sh
+++ vimrc_update.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Make sure this file is executable with: chmod u+x vi_setup.sh
+# Run this file with ./vimrc_update.sh
+
 echo "************************"
 echo " Update vimrc for VI ..."
 echo "************************"
 date
 
-# Download the vimrc file
+# Download the latest vimrc file from github
 wget https://raw.githubusercontent.com/srosendal/vimrc/master/vimrc
 
+# Replace vimrc file
 echo "Replacing original vimrc with personal vimrc"
 sudo mv vimrc /etc/vim/vimrc
 echo "All Done !"
